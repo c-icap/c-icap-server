4f72dfceda38b6ce704b99fe8eb8b938c07c6284
chtsanti <chtsanti@950cb5d8-81e4-40ab-8cb8-c719d5138fe1>
2006-06-14 18:52:37 +0000

    Elisey O. Savateev reports that double ; causes problems to FreeBSD 4.x and possibly to old gcc compilers
    git-svn-id: https://svn.code.sf.net/p/c-icap/code/c-icap-server/trunk/c-icap@102 950cb5d8-81e4-40ab-8cb8-c719d5138fe1

--- request_common.c
+++ request_common.c
@@ -654,7 +654,7 @@ static int check_realloc(char **buf,int *size,int used,int mustadded){
 
 
 int client_parse_icap_header(request_t *req,ci_headers_list_t *h){
-     int readed=0,eoh=0;;
+     int readed=0,eoh=0;
      char *buf,*end;
      if(req->pstrblock_read_len<4)/*we need 4 bytes for the end of headers "\r\n\r\n" string*/
 	  return CI_NEEDS_MORE;
