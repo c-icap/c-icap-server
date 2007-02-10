d2f05f80e275cd035fb69db1535bac7f0a85ecdb
chtsanti <chtsanti@950cb5d8-81e4-40ab-8cb8-c719d5138fe1>
2007-02-10 21:40:08 +0000

    formating ....
    git-svn-id: https://svn.code.sf.net/p/c-icap/code/c-icap-server/trunk/c-icap@148 950cb5d8-81e4-40ab-8cb8-c719d5138fe1

--- os/win32/dll_entry.c
+++ os/win32/dll_entry.c
@@ -1,10 +1,8 @@
 
 #include <Windows.h>
 
- BOOL WINAPI _DllMainCRTStartup(HINSTANCE hinstDLL, DWORD fdwReason,
-                                 LPVOID lpReserved) 
+BOOL WINAPI _DllMainCRTStartup(HINSTANCE hinstDLL, DWORD fdwReason,
+                               LPVOID lpReserved)
 {
-     return (TRUE);
-}
-
-
+     return (TRUE);
+}
