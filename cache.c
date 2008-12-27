22ac7c465156a3bd930597f4364145e1cddb97ac
chtsanti <chtsanti@950cb5d8-81e4-40ab-8cb8-c719d5138fe1>
2008-12-27 16:26:04 +0000

    Changes: - Remove the keylen member from the ci_cache_entry struct. We should be able   to compute the len of the key member. - Remove the compare method from struct ci_cache_table. - From now use a new member the "key_ops" of type ci_type_ops to define   the "operators" (compare, find length etc) to use with keys - Add the ci_cache_table::data_release member which is a the function used to   release the data of a stored key/data pair - Use a allocator object for allocating memory for - Definitions of struct ci_cache_entry and struct ci_cache_table moved to   cache.h file - Remove the test code inside "NOT_USED" #ifdef block - a number of changes
    git-svn-id: https://svn.code.sf.net/p/c-icap/code/c-icap-server/trunk/c-icap@287 950cb5d8-81e4-40ab-8cb8-c719d5138fe1

--- cache.c
+++ cache.c
@@ -2,230 +2,122 @@
 #include <stdlib.h>
 #include <time.h>
 #include "debug.h"
+#include "lookup_table.h"
+#include "cache.h"
 
-
-struct ci_cache_entry {
-   unsigned int hash;
-   time_t time;
-   void *key;
-   int keylen;
-   void *val;
-   struct ci_cache_entry *qnext;
-   struct ci_cache_entry *hnext;
-};
-
-
-#define CI_CACHE_CONT_MEMBLOCK       0x1
-#define CI_CACHE_ALLOC_MEMOBJECTS    0x2
-
-struct ci_cache_table { 
-    struct ci_cache_entry *first_queue_entry; 
-    struct ci_cache_entry *last_queue_entry;
-    struct ci_cache_entry **hash_table;
-    time_t ttl;
-    unsigned int cache_size;
-    unsigned int hash_table_size;
-    unsigned int flags;
-    int (*compare)(void *key1, int keylen1, void *key2, int keylen2);
-    void (*release) (void *key,void *val);
-};
 
 time_t ci_internal_time()
 {
     return time(NULL);
 }
 
-
-unsigned int compute_hash(struct ci_cache_table *cache, void *key, int len){
-    unsigned long hash = 5381;
-    unsigned char *s = key;
-    int i;
-
-    if(len) {
-	for (i=0; i<len; i++,s++)
-	    hash = ((hash << 5) + hash) + *s;
-    }
-    else
-	while (*s) {
-	    hash = ((hash << 5) + hash) + *s; /* hash * 33 + current char */
-	    s++;
-	}
-    
-    if(hash==0) hash++;
-    hash = hash & cache->hash_table_size; /*Keep only the bits we need*/
-    return hash;
-}
-
-struct ci_cache_table *ci_cache_build(unsigned int hash_size,
-				unsigned int cache_size, 
-				int ttl,
-				unsigned int flags,
-				int (*compare)(void *, int ,void *, int ),
-				void (*release) (void *key,void *val)) {
+struct ci_cache_table *ci_cache_build( unsigned int cache_size, 
+				       int ttl,
+				       unsigned int flags,
+				       ci_type_ops_t *ops,
+				       void (*data_release)(void *val, ci_mem_allocator_t *allocator),
+				       ci_mem_allocator_t *allocator ) {
    struct ci_cache_table *cache;
    int i;
    unsigned int new_hash_size;
+
+   if(cache_size <= 0 || cache_size>65535)
+       return NULL;
+
    cache = malloc(sizeof(struct ci_cache_table));
    cache->flags = flags;
-
-    if(cache_size) {
-	if(cache->flags &  CI_CACHE_CONT_MEMBLOCK) {
-	    /*Allocating continue memory block for cache, maybe we use it in a 
-	      shared memory...*/ 
-	    cache->first_queue_entry = calloc(cache_size,sizeof(struct ci_cache_entry));
-	    for (i=0; i<cache_size-1; i++)
-		cache->first_queue_entry[i].qnext = &(cache->first_queue_entry[i+1]);
-	    cache->first_queue_entry[cache_size-1].qnext = NULL;      
-	    cache->last_queue_entry=&(cache->first_queue_entry[cache_size-1]);
-	}
-	else {
-	    cache->first_queue_entry = calloc(1,sizeof(struct ci_cache_entry)); 
-	    cache->last_queue_entry = cache->first_queue_entry;
-	    for (i=0; i < cache_size-1; i++) {
-		cache->last_queue_entry->qnext=calloc(1,sizeof(struct ci_cache_entry));
-		cache->last_queue_entry=cache->last_queue_entry->qnext;
-	    }
-	}
-   }
-   else {
-        cache->first_queue_entry = NULL;
-        cache->last_queue_entry = NULL;
-   }
+   cache->key_ops = ops;
+   cache->allocator = allocator;
+   cache->data_release = data_release;
    cache->cache_size = cache_size;
+
+   cache->first_queue_entry = (struct ci_cache_entry *)allocator->alloc(allocator, sizeof(struct ci_cache_entry)); 
+   cache->last_queue_entry = cache->first_queue_entry;
+   for (i=0; i < cache_size-1; i++) {
+       cache->last_queue_entry->hnext=NULL;
+       cache->last_queue_entry->qnext=(struct ci_cache_entry *)allocator->alloc(allocator, sizeof(struct ci_cache_entry));
+       cache->last_queue_entry=cache->last_queue_entry->qnext;
+   }
+
 
 
    new_hash_size = 63;
-   if(hash_size > 63) {
-       while(new_hash_size<hash_size && new_hash_size < 0xFFFFFF){
+   if(cache_size > 63) {
+       while(new_hash_size<cache_size && new_hash_size < 0xFFFFFF){
 	   new_hash_size++; 
 	   new_hash_size = (new_hash_size << 1) -1;
        }
    }
    ci_debug_printf(7,"Hash size: %d\n",new_hash_size);
-   cache->hash_table=calloc(new_hash_size,sizeof(struct ci_cache_entry *));
+   cache->hash_table=(struct ci_cache_entry **)allocator->alloc(allocator, new_hash_size*sizeof(struct ci_cache_entry *));
+   memset(cache->hash_table,0,new_hash_size*sizeof(struct ci_cache_entry *));
    cache->hash_table_size = new_hash_size; 
    cache->ttl = ttl;
-   cache->compare = compare;
-   cache->release = release;
    return cache;
 }
 
-void *ci_cache_search(struct ci_cache_table *cache,void *key,int key_len) {
+void *ci_cache_search(struct ci_cache_table *cache,void *key) {
     struct ci_cache_entry *e;
-    unsigned int hash=compute_hash(cache, key, key_len);
+    unsigned int hash=ci_hash_compute(cache->hash_table_size, key, cache->key_ops->size(key));
 
     if(hash >= cache->hash_table_size) /*is it possible?*/
 	return NULL;
 
     e=cache->hash_table[hash];
     while(e!=NULL) {
-	ci_debug_printf(10," \t\t->>>>Val %s\n",e->val);
-	if(cache->compare(e->key, e->keylen, key, key_len))
+	ci_debug_printf(10," \t\t->>>>Val %s\n",(char *)e->val);
+	ci_debug_printf(10," \t\t->>>>compare %s ~ %s\n",(char *)e->key, (char *)key);
+	if(cache->key_ops->compare(e->key, key) == 0)
            return e->val;
        e=e->hnext;
     }
    return NULL;
 }
 
-void *ci_cache_update(struct ci_cache_table *cache, void *key, int key_len, void *val) {
+void *ci_cache_update(struct ci_cache_table *cache, void *key, void *val) {
     struct ci_cache_entry *e,*tmp;
-    unsigned int hash=compute_hash(cache, key, key_len);
+    unsigned int hash=ci_hash_compute(cache->hash_table_size, key, cache->key_ops->size(key));
 
-    ci_debug_printf(10,"Adding :%s:%s\n",key,val);
+    ci_debug_printf(10,"Adding :%s:%s\n",(char *)key, (char *)val);
 
-    if (cache->cache_size) { /*It is a cache with static size */
-        /*Get the oldest entry (TODO:check the cache ttl value if exists)*/
-    	e=cache->first_queue_entry;
-    	cache->first_queue_entry=cache->first_queue_entry->qnext;
-        /*Make it the newest entry (make it last entry in queue)*/
-        cache->last_queue_entry->qnext = e;
-        cache->last_queue_entry = e;
-        e->qnext = NULL;
-            
-        /*If it has data release its data*/
-    	if (e->key)
-	     cache->release(e->key, e->val);
-        /*If it is in the hash table remove it...*/
-    	if(e->hash) {
-		tmp = cache->hash_table[e->hash];
-		if(tmp == e)
-	    		cache->hash_table[e->hash] = tmp->hnext;
-		else if(tmp) {
-	    		while(tmp->hnext != NULL && e != tmp->hnext) tmp = tmp->hnext;
-	    		if(tmp->hnext)
-				tmp->hnext = tmp->hnext->hnext;
-		}
-	}
-        
-    }
-    else { /*is a cache with variable size so just allocate new entry and put it to hash*/
-        e = malloc(sizeof(struct ci_cache_entry));
-        e->qnext = NULL; /*not used here*/
-    }
+    /*Get the oldest entry (TODO:check the cache ttl value if exists)*/
+    e=cache->first_queue_entry;
+    cache->first_queue_entry=cache->first_queue_entry->qnext;
+    /*Make it the newest entry (make it last entry in queue)*/
+    cache->last_queue_entry->qnext = e;
+    cache->last_queue_entry = e;
+    e->qnext = NULL;
     
+    /*If it has data release its data*/
+    if (e->key)
+	cache->key_ops->free(e->key, cache->allocator);
+    if (cache->data_release)
+	cache->data_release(e->val, cache->allocator);
+
+    /*If it is in the hash table remove it...*/
+    if(e->hash) {
+	tmp = cache->hash_table[e->hash];
+	if(tmp == e)
+	    cache->hash_table[e->hash] = tmp->hnext;
+	else if(tmp) {
+	    while(tmp->hnext != NULL && e != tmp->hnext) tmp = tmp->hnext;
+	    if(tmp->hnext)
+		tmp->hnext = tmp->hnext->hnext;
+	}
+    }
     
     e->hnext = NULL;
     e->time = ci_internal_time();
     e->key=key;
-    e->keylen=key_len;
     e->val=val;
     e->hash=hash;
    
 
     if(cache->hash_table[hash])
-	ci_debug_printf(10,"\t\t:::Found %s\n",cache->hash_table[hash]->val);
+	ci_debug_printf(10,"\t\t:::Found %s\n", cache->hash_table[hash]->val);
     /*Make it the first entry in the current hash entry*/
     e->hnext=cache->hash_table[hash];
     cache->hash_table[hash] = e;
 }
 
 
-#ifdef NOT_USED
-
-int strmatch(void *key1, int keylen1, void *key2, int keylen2)
-{
-    char *skey1=(char *)key1, *skey2=(char *)key2;
-    return (strcmp(skey1,skey2)==0);
-}
-
-void strrelease(void *key, void *val){
-    printf("Freeing mem for :%s\n",(char *)val);
-    free(val);
-}
-
-
-int main(int argc,char *argv[]) {
-    struct ci_cache_table *cache;
-    char *s;
-    cache = ci_cache_build(3,3,10,0,strmatch,strrelease);
-
-    s=strdup("test1");
-    ci_cache_update(cache,s,0,s);
-
-    s=strdup("test2");
-    ci_cache_update(cache,s,0,s);
-
-    s=strdup("test3");
-    ci_cache_update(cache,s,0,s);
-
-
-    s=strdup("test4");
-    cache_update(cache,s,0,s);
-
-
-    s=ci_cache_search(cache,"test2",0);
-    printf("Found : %s\n",s);
-
-    s=ci_cache_search(cache,"test21",0);
-    printf("Found : %s\n",s);
-
-    s=ci_cache_search(cache,"test1",0);
-    printf("Found : %s\n",s);
-
-    s=ci_cache_search(cache,"test4",0);
-    printf("Found : %s\n",s);
-
-}
-
-
-#endif
---
+++ include/cache.h
@@ -0,0 +1,57 @@
+/*
+ *  Copyright (C) 2004-2008 Christos Tsantilas
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA  02110-1301  USA.
+ */
+
+#ifndef __CACHE_H
+#define __CACHE_H
+#include "hash.h"
+
+struct ci_cache_entry {
+   unsigned int hash;
+   time_t time;
+   void *key;
+   void *val;
+   struct ci_cache_entry *qnext;
+   struct ci_cache_entry *hnext;
+};
+
+
+struct ci_cache_table { 
+    struct ci_cache_entry *first_queue_entry; 
+    struct ci_cache_entry *last_queue_entry;
+    struct ci_cache_entry **hash_table;
+    time_t ttl;
+    unsigned int cache_size;
+    unsigned int hash_table_size;
+    unsigned int flags;
+    ci_type_ops_t *key_ops;
+    ci_mem_allocator_t *allocator;
+    void (*data_release)(void *val, ci_mem_allocator_t *allocator);
+};
+
+CI_DECLARE_FUNC(struct ci_cache_table *) ci_cache_build( unsigned int cache_size, 
+							 int ttl,
+							 unsigned int flags,
+							 ci_type_ops_t *ops,
+							 void (*data_release)(void *val, ci_mem_allocator_t *allocator),
+							 ci_mem_allocator_t *allocator );
+
+CI_DECLARE_FUNC(void *) ci_cache_search(struct ci_cache_table *cache,void *key);
+CI_DECLARE_FUNC(void *) ci_cache_update(struct ci_cache_table *cache, void *key, void *val);
+
+#endif
