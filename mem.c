658257c96f41bd8385f4845371bafd538d9eea27
chtsanti <chtsanti@950cb5d8-81e4-40ab-8cb8-c719d5138fe1>
2009-01-10 21:01:31 +0000

    - Fix the ci_create_pool_allocator function. - ci_create_pool_allocator now takes as argument the pool stored objects size and the allocator to use to allocate memory for pool objects
    git-svn-id: https://svn.code.sf.net/p/c-icap/code/c-icap-server/trunk/c-icap@297 950cb5d8-81e4-40ab-8cb8-c719d5138fe1

--- mem.c
+++ mem.c
@@ -262,158 +262,163 @@ ci_mem_allocator_t *ci_create_pack_allocator(char *memblock, int size)
 /****************************************************************/
 
 struct mem_block_item {
-  void *data;
-  struct mem_block_item *next;
+    void *data;
+    struct mem_block_item *next;
 };
 
 struct pool_allocator {
-  int items_size;
-  int strict;
-  ci_mem_allocator_t *allocator;
-  int free_allocator;
-  ci_thread_mutex_t mutex;
-  struct mem_block_item *free;
-  struct mem_block_item *allocated;
+    int items_size;
+    int strict;
+    ci_mem_allocator_t *allocator;
+    int free_allocator;
+    ci_thread_mutex_t mutex;
+    struct mem_block_item *free;
+    struct mem_block_item *allocated;
 };
 
 struct pool_allocator *pool_allocator_build(int items_size, 
 					    int strict,
 					    ci_mem_allocator_t *use_alloc)
 {
-  int free_allocator = 0; 
-  struct pool_allocator *palloc;
-  
-  if(!use_alloc) {
-      free_allocator = 1;
-      use_alloc = ci_create_os_allocator();
-  }
-
-  if(!use_alloc) {
-      /*a debug message*/
-      return NULL;
-  }
-  palloc = use_alloc->alloc(use_alloc, sizeof(struct pool_allocator));
-  
-  if(!palloc) {
-      /*An error message*/
-      if(free_allocator)
-	  ci_mem_allocator_destroy(use_alloc);
-      return NULL;
-  }
-
-  palloc->items_size = items_size;
-  palloc->strict = strict;
-  palloc->free = NULL;
-  palloc->allocated = NULL;
-  palloc->free_allocator = free_allocator; 
-  palloc->allocator = use_alloc;
-  ci_thread_mutex_init(&palloc->mutex);
-  return palloc;
+    int free_allocator = 0; 
+    struct pool_allocator *palloc;
+    
+    if(!use_alloc) {
+	free_allocator = 1;
+	use_alloc = ci_create_os_allocator();
+    }
+    
+    if(!use_alloc) {
+	/*a debug message*/
+	return NULL;
+    }
+    palloc = use_alloc->alloc(use_alloc, sizeof(struct pool_allocator));
+    
+    if(!palloc) {
+	/*An error message*/
+	if(free_allocator)
+	    ci_mem_allocator_destroy(use_alloc);
+	return NULL;
+    }
+    
+    palloc->items_size = items_size;
+    palloc->strict = strict;
+    palloc->free = NULL;
+    palloc->allocated = NULL;
+    palloc->free_allocator = free_allocator; 
+    palloc->allocator = use_alloc;
+    ci_thread_mutex_init(&palloc->mutex);
+    return palloc;
 }
 
 void *pool_allocator_alloc(ci_mem_allocator_t *allocator,size_t size)
 {
-  struct mem_block_item *mem_item;
-  void *data = NULL;
-  struct pool_allocator *palloc = (struct pool_allocator *)allocator->data;
-
-  if(size > palloc->items_size)
-      return NULL;
-
-  ci_thread_mutex_lock(&palloc->mutex);
-
-  if(palloc->free) {
-    mem_item=palloc->free;
-    palloc->free=palloc->free->next;
-    data=mem_item->data;
-    mem_item->data=NULL;
-  }
-  else {
-    mem_item = palloc->allocator->alloc(palloc->allocator, 
-					sizeof(struct mem_block_item));
-    mem_item->data=NULL;
-    data = palloc->allocator->alloc(palloc->allocator, palloc->items_size);
-  }
- 
-  mem_item->next=palloc->allocated;
-  palloc->allocated = mem_item;
-
-  ci_thread_mutex_unlock(&palloc->mutex);
-  return data;
+    struct mem_block_item *mem_item;
+    void *data = NULL;
+    struct pool_allocator *palloc = (struct pool_allocator *)allocator->data;
+    
+    if(size > palloc->items_size)
+	return NULL;
+    
+    ci_thread_mutex_lock(&palloc->mutex);
+    
+    if(palloc->free) {
+	mem_item=palloc->free;
+	palloc->free=palloc->free->next;
+	data=mem_item->data;
+	mem_item->data=NULL;
+    }
+    else {
+	mem_item = palloc->allocator->alloc(palloc->allocator, 
+					    sizeof(struct mem_block_item));
+	mem_item->data=NULL;
+	data = palloc->allocator->alloc(palloc->allocator, palloc->items_size);
+    }
+    
+    mem_item->next=palloc->allocated;
+    palloc->allocated = mem_item;
+    
+    ci_thread_mutex_unlock(&palloc->mutex);
+    return data;
 }
 
 void pool_allocator_free(ci_mem_allocator_t *allocator,void *p)
 {
-  struct mem_block_item *mem_item;
-  struct pool_allocator *palloc = (struct pool_allocator *)allocator->data;
-  
-  ci_thread_mutex_lock(&palloc->mutex);
-  if(!palloc->allocated) {
-    /*Yes can happen! after a reset but users did not free all objects*/
-    free(p);
-  }
-  else {
-    mem_item=palloc->allocated;
-    palloc->allocated = palloc->allocated->next;
+    struct mem_block_item *mem_item;
+    struct pool_allocator *palloc = (struct pool_allocator *)allocator->data;
     
-    mem_item->data = p;
-    mem_item->next = palloc->free;
-    palloc->free = mem_item;
-  }
-  ci_thread_mutex_unlock(&palloc->mutex);
+    ci_thread_mutex_lock(&palloc->mutex);
+    if(!palloc->allocated) {
+	/*Yes can happen! after a reset but users did not free all objects*/
+	free(p);
+    }
+    else {
+	mem_item=palloc->allocated;
+	palloc->allocated = palloc->allocated->next;
+	
+	mem_item->data = p;
+	mem_item->next = palloc->free;
+	palloc->free = mem_item;
+    }
+    ci_thread_mutex_unlock(&palloc->mutex);
 }
 
 void pool_allocator_reset(ci_mem_allocator_t *allocator)
 {
-  struct mem_block_item *mem_item, *cur;
-  struct pool_allocator *palloc = (struct pool_allocator *)allocator->data;
-  
-  ci_thread_mutex_lock(&palloc->mutex);
-  if(palloc->allocated) {
-    mem_item = palloc->allocated;
-    while(mem_item!=NULL) {
-      cur = mem_item;
-      mem_item = mem_item->next;
-      palloc->allocator->free(palloc->allocator, cur);
+    struct mem_block_item *mem_item, *cur;
+    struct pool_allocator *palloc = (struct pool_allocator *)allocator->data;
+    
+    ci_thread_mutex_lock(&palloc->mutex);
+    if(palloc->allocated) {
+	mem_item = palloc->allocated;
+	while(mem_item!=NULL) {
+	    cur = mem_item;
+	    mem_item = mem_item->next;
+	    palloc->allocator->free(palloc->allocator, cur);
+	}
+	
     }
-      
-  }
-  palloc->allocated = NULL;
-  if(palloc->free) {
-    mem_item = palloc->free;
-    while(mem_item!=NULL) {
-      cur = mem_item;
-      mem_item = mem_item->next;
-      palloc->allocator->free(palloc->allocator, cur->data);
-      palloc->allocator->free(palloc->allocator, cur);
+    palloc->allocated = NULL;
+    if(palloc->free) {
+	mem_item = palloc->free;
+	while(mem_item!=NULL) {
+	    cur = mem_item;
+	    mem_item = mem_item->next;
+	    palloc->allocator->free(palloc->allocator, cur->data);
+	    palloc->allocator->free(palloc->allocator, cur);
+	}
     }
-  }
-  palloc->free = NULL;
-  ci_thread_mutex_unlock(&palloc->mutex);
+    palloc->free = NULL;
+    ci_thread_mutex_unlock(&palloc->mutex);
 }
 
 
 void pool_allocator_destroy(ci_mem_allocator_t *allocator)
 {
-  int free_allocator = 0;
-  pool_allocator_reset(allocator);
-  struct pool_allocator *palloc = (struct pool_allocator *)allocator->data;
-  ci_mem_allocator_t *use_alloc = palloc->allocator;
-  free_allocator = palloc->free_allocator;
-  use_alloc->free(use_alloc, palloc);
-  if(free_allocator)
-      ci_mem_allocator_destroy(use_alloc);
+    int free_allocator = 0;
+    pool_allocator_reset(allocator);
+    struct pool_allocator *palloc = (struct pool_allocator *)allocator->data;
+    ci_mem_allocator_t *use_alloc = palloc->allocator;
+    ci_thread_mutex_destroy(&palloc->mutex);
+    free_allocator = palloc->free_allocator;
+    use_alloc->free(use_alloc, palloc);
+    if(free_allocator)
+	ci_mem_allocator_destroy(use_alloc);
 }
 
-ci_mem_allocator_t *ci_create_pool_allocator(int size)
+ci_mem_allocator_t *ci_create_pool_allocator(int items_size, ci_mem_allocator_t *use_alloc)
 {
-  ci_mem_allocator_t *allocator = malloc(sizeof(ci_mem_allocator_t));
-  if(!allocator)
-    return NULL;
-  allocator->alloc = pool_allocator_alloc;
-  allocator->free = pool_allocator_free;
-  allocator->reset = pool_allocator_reset;
-  allocator->destroy = pool_allocator_destroy;
-  allocator->data = NULL;
-  return allocator;
+    struct pool_allocator *palloc;
+    ci_mem_allocator_t *allocator;
+    
+    palloc = pool_allocator_build(items_size, 0, use_alloc);
+    allocator= malloc(sizeof(ci_mem_allocator_t));
+    if (!allocator)
+	return NULL;
+    allocator->alloc = pool_allocator_alloc;
+    allocator->free = pool_allocator_free;
+    allocator->reset = pool_allocator_reset;
+    allocator->destroy = pool_allocator_destroy;
+    allocator->data = palloc;
+    return allocator;
 }
