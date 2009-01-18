594ce1c781fac4b684fc25fd231bdffe150c924c
chtsanti <chtsanti@950cb5d8-81e4-40ab-8cb8-c719d5138fe1>
2009-01-18 17:06:11 +0000

    - rename the ci_cache_table to ci_cache  - Add multithread support for ci_cache  - The copy_to/copy_from takes as argument the value data size too  - A number of bugs fixed
    git-svn-id: https://svn.code.sf.net/p/c-icap/code/c-icap-server/trunk/c-icap@302 950cb5d8-81e4-40ab-8cb8-c719d5138fe1

--- cache.c
+++ cache.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "debug.h"
+#include "ci_threads.h"
 #include "lookup_table.h"
 #include "cache.h"
 
@@ -11,49 +12,95 @@ time_t ci_internal_time()
     return time(NULL);
 }
 
-struct ci_cache_table *ci_cache_build( unsigned int cache_size,
-				       unsigned int max_object_size,
-				       int ttl,
-				       ci_type_ops_t *key_ops,
-				       void *(copy_to)(void *val, ci_mem_allocator_t *allocator),
-				       void *(copy_from)(void *val, ci_mem_allocator_t *allocator)
+
+int common_mutex_init(common_mutex_t *mtx, int proc_mtx)
+{
+    if(proc_mtx)
+	return 0;
+    
+    mtx->isproc = 0;
+    return ci_thread_mutex_init(&mtx->mtx.thread_mutex);    
+}
+
+int common_mutex_destroy(common_mutex_t *mtx)
+{
+    if(mtx->isproc)
+	return 0;
+    return ci_thread_mutex_destroy(&mtx->mtx.thread_mutex);
+}
+
+int common_mutex_lock(common_mutex_t *mtx)
+{
+    if(mtx->isproc)
+	return 0;
+    return ci_thread_mutex_lock(&mtx->mtx.thread_mutex);
+}
+
+int common_mutex_unlock(common_mutex_t *mtx)
+{
+    if(mtx->isproc)
+	return 0;
+    return ci_thread_mutex_unlock(&mtx->mtx.thread_mutex);
+}
+
+
+struct ci_cache *ci_cache_build( unsigned int cache_size,
+				 unsigned int max_key_size,
+				 unsigned int max_object_size,
+				 int ttl,
+				 ci_type_ops_t *key_ops,
+				 void *(copy_to_cache)(void *val,int *val_size, ci_mem_allocator_t *allocator),
+				 void *(copy_from_cache)(void *val, int val_size, ci_mem_allocator_t *allocator)
     ) 
 {
-    struct ci_cache_table *cache;
+    struct ci_cache *cache;
     int i;
-    unsigned int new_hash_size;
+    unsigned int new_hash_size, cache_items;
     ci_mem_allocator_t *allocator;
+    struct ci_cache_entry *e;
     
-    if(cache_size <= 0 || cache_size > 65535)
+    if (cache_size <= 0)
+	return NULL;
+
+    cache_items = cache_size/(max_key_size+max_object_size+sizeof(struct ci_cache_entry));
+    if (cache_items == 0)
 	return NULL;
 
     /*until we are going to create an allocator which can allocate/release from 
      continues memory blocks like those we have in shared memory*/
     allocator = ci_create_os_allocator();
-
     
-    cache = malloc(sizeof(struct ci_cache_table));
+    cache = malloc(sizeof(struct ci_cache));
     cache->key_ops = key_ops;
     cache->allocator = allocator;
 //    cache->data_release = data_release;
-    cache->cache_size = cache_size;
+    cache->cache_size = cache_items;
+    cache->mem_size = cache_size;
+    cache->max_key_size = max_key_size;
     cache->max_object_size = max_object_size;
-    cache->copy_to = copy_to;
-    cache->copy_from = copy_from;
+    cache->copy_to = copy_to_cache;
+    cache->copy_from = copy_from_cache;
     
     cache->first_queue_entry = (struct ci_cache_entry *)allocator->alloc(allocator, sizeof(struct ci_cache_entry)); 
     cache->last_queue_entry = cache->first_queue_entry;
-    for (i=0; i < cache_size-1; i++) {
-	cache->last_queue_entry->hnext=NULL;
+    cache->last_queue_entry->hnext=NULL;
+    cache->last_queue_entry->key = NULL;
+    cache->last_queue_entry->val = NULL;
+    cache->last_queue_entry->time = 0;
+    cache->last_queue_entry->hash = 0;
+    for (i=0; i < cache_items-1; i++) {
 	cache->last_queue_entry->qnext=(struct ci_cache_entry *)allocator->alloc(allocator, sizeof(struct ci_cache_entry));
 	cache->last_queue_entry=cache->last_queue_entry->qnext;
+	cache->last_queue_entry->hnext=NULL;
+	cache->last_queue_entry->key = NULL;
+	cache->last_queue_entry->val = NULL;
+	cache->last_queue_entry->time = 0;
+	cache->last_queue_entry->hash = 0;
     }
     
-    
-    
     new_hash_size = 63;
-    if(cache_size > 63) {
-	while(new_hash_size<cache_size && new_hash_size < 0xFFFFFF){
+    if(cache_items > 63) {
+	while(new_hash_size<cache_items && new_hash_size < 0xFFFFFF){
 	    new_hash_size++; 
 	    new_hash_size = (new_hash_size << 1) -1;
 	}
@@ -63,10 +110,12 @@ struct ci_cache_table *ci_cache_build( unsigned int cache_size,
     memset(cache->hash_table,0,new_hash_size*sizeof(struct ci_cache_entry *));
     cache->hash_table_size = new_hash_size; 
     cache->ttl = ttl;
+    common_mutex_init(&cache->mtx, 0);
+
     return cache;
 }
 
-void ci_cache_destroy(struct ci_cache_table *cache) 
+void ci_cache_destroy(struct ci_cache *cache) 
 {
     struct ci_cache_entry *e;
     e = cache->first_queue_entry;
@@ -80,30 +129,34 @@ void ci_cache_destroy(struct ci_cache_table *cache)
 	e = cache->first_queue_entry;
     }
     cache->allocator->free(cache->allocator, cache->hash_table);
+    common_mutex_destroy(&cache->mtx);
     free(cache);
 }
 
-void *ci_cache_search(struct ci_cache_table *cache,void *key, void **val, ci_mem_allocator_t *val_allocator) {
+void *ci_cache_search(struct ci_cache *cache,void *key, void **val, ci_mem_allocator_t *val_allocator) {
     struct ci_cache_entry *e;
     unsigned int hash=ci_hash_compute(cache->hash_table_size, key, cache->key_ops->size(key));
     
     if(hash >= cache->hash_table_size) /*is it possible?*/
 	return NULL;
 
+    common_mutex_lock(&cache->mtx);
     e=cache->hash_table[hash];
     while(e != NULL) {
 	ci_debug_printf(10," \t\t->>>>Val %s\n",(char *)e->val);
 	ci_debug_printf(10," \t\t->>>>compare %s ~ %s\n",(char *)e->key, (char *)key);
 	if(cache->key_ops->compare(e->key, key) == 0) {
-	    *val = cache->copy_from(e->val, val_allocator);
+	    *val = cache->copy_from(e->val, e->val_size, val_allocator);
+	    common_mutex_unlock(&cache->mtx);
 	    return key;
 	}
 	e = e->hnext;
     }
-   return NULL;
+    common_mutex_unlock(&cache->mtx);
+    return NULL;
 }
 
-int ci_cache_update(struct ci_cache_table *cache, void *key, void *val) {
+int ci_cache_update(struct ci_cache *cache, void *key, void *val) {
     struct ci_cache_entry *e,*tmp;
     int key_size;
     time_t current_time;
@@ -111,13 +164,20 @@ int ci_cache_update(struct ci_cache_table *cache, void *key, void *val) {
 
     ci_debug_printf(10,"Adding :%s:%s\n",(char *)key, (char *)val);
 
+    current_time = ci_internal_time();
+
+    common_mutex_lock(&cache->mtx);
     /*Get the oldest entry*/
     e=cache->first_queue_entry;
-    current_time = ci_internal_time();
     
     /*if the oldest entry does not expired do not store tke key/value pair*/
-    if((current_time - e->time)< cache->ttl)
+    if((current_time - e->time)< cache->ttl) {
+	ci_debug_printf(6, "ci_cache_update: not available slot (%d-%d %d).\n",
+			current_time,  e->time, cache->ttl
+	    );
+	common_mutex_unlock(&cache->mtx);
 	return 0;
+    }
     
     /*If it has data release its data*/
     if (e->key) {
@@ -148,13 +208,19 @@ int ci_cache_update(struct ci_cache_table *cache, void *key, void *val) {
     /*I should implement a ci_type_ops::clone method. Maybe the memcpy is not enough....*/
     key_size = cache->key_ops->size(key);
     e->key = cache->allocator->alloc(cache->allocator, key_size);
-    if(!e->key)
+    if(!e->key) {
+	common_mutex_unlock(&cache->mtx);
+	ci_debug_printf(6, "ci_cache_update: failed to allocate memory for key.\n");
 	return 0;
+    }
     memcpy(e->key, key, key_size);
 
-    e->val = cache->copy_to(val, cache->allocator);
+    e->val = cache->copy_to(val, &e->val_size, cache->allocator);
     if(!e->val) {
 	cache->allocator->free(cache->allocator, e->key);
+	e->key = NULL;
+	common_mutex_unlock(&cache->mtx);
+	ci_debug_printf(6, "ci_cache_update: failed to allocate memory for cache data.\n");
 	return 0;
     }
 
@@ -171,6 +237,8 @@ int ci_cache_update(struct ci_cache_table *cache, void *key, void *val) {
     /*Make it the first entry in the current hash entry*/
     e->hnext=cache->hash_table[hash];
     cache->hash_table[hash] = e;
+
+    common_mutex_unlock(&cache->mtx);
     return 1;
 }
 
--- include/cache.h
+++ include/cache.h
@@ -20,43 +20,58 @@
 #ifndef __CACHE_H
 #define __CACHE_H
 #include "hash.h"
+#include "proc_mutex.h"
+#include "ci_threads.h"
 
 struct ci_cache_entry {
    unsigned int hash;
    time_t time;
    void *key;
    void *val;
+   int val_size;
    struct ci_cache_entry *qnext;
    struct ci_cache_entry *hnext;
 };
 
+typedef struct common_mutex {
+    int isproc;
+    union {
+	ci_proc_mutex_t proc_mutex;
+	ci_thread_mutex_t thread_mutex;
+    } mtx;
+} common_mutex_t;
 
-struct ci_cache_table { 
+
+typedef struct ci_cache{ 
     struct ci_cache_entry *first_queue_entry; 
     struct ci_cache_entry *last_queue_entry;
     struct ci_cache_entry **hash_table;
     time_t ttl;
     unsigned int cache_size;
+    unsigned int mem_size;
+    unsigned int max_key_size;
     unsigned int max_object_size;
     unsigned int hash_table_size;
     unsigned int flags;
     ci_type_ops_t *key_ops;
     ci_mem_allocator_t *allocator;
-    void *(*copy_to)(void *val, ci_mem_allocator_t *allocator);
-    void *(*copy_from)(void *val, ci_mem_allocator_t *allocator);
+    common_mutex_t mtx;
+    void *(*copy_to)(void *val,int *val_size, ci_mem_allocator_t *allocator);
+    void *(*copy_from)(void *val,int val_size, ci_mem_allocator_t *allocator);
 //    void (*data_release)(void *val, ci_mem_allocator_t *allocator);
-};
+} ci_cache_t;
 
-CI_DECLARE_FUNC(struct ci_cache_table *) ci_cache_build( unsigned int cache_size,
- 							 unsigned int max_object_size,
-							 int ttl,
-							 ci_type_ops_t *key_ops,
-							 void *(copy_to)(void *val, ci_mem_allocator_t *allocator),
-							 void *(copy_from)(void *val, ci_mem_allocator_t *allocator)
+CI_DECLARE_FUNC(struct ci_cache *) ci_cache_build( unsigned int cache_size,
+						   unsigned int max_object_size,
+						   unsigned int max_key_size,
+						   int ttl,
+						   ci_type_ops_t *key_ops,
+						   void *(copy_to_cache)(void *val, int *val_size, ci_mem_allocator_t *allocator),
+						   void *(copy_from_cache)(void *val,int val_size, ci_mem_allocator_t *allocator)
     );
 
-CI_DECLARE_FUNC(void *) ci_cache_search(struct ci_cache_table *cache,void *key, void **val, ci_mem_allocator_t *allocator);
-CI_DECLARE_FUNC(int) ci_cache_update(struct ci_cache_table *cache, void *key, void *val);
-CI_DECLARE_FUNC(void) ci_cache_destroy(struct ci_cache_table *cache);
+CI_DECLARE_FUNC(void *) ci_cache_search(struct ci_cache *cache,void *key, void **val, ci_mem_allocator_t *allocator);
+CI_DECLARE_FUNC(int) ci_cache_update(struct ci_cache *cache, void *key, void *val);
+CI_DECLARE_FUNC(void) ci_cache_destroy(struct ci_cache *cache);
 
 #endif
