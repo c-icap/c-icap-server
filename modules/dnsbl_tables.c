b99a6df1759b0da9d94c924775ad100f74c368bd
chtsanti <chtsanti@950cb5d8-81e4-40ab-8cb8-c719d5138fe1>
2009-03-15 16:49:31 +0000

    Add support for DNS black/white lists
    git-svn-id: https://svn.code.sf.net/p/c-icap/code/c-icap-server/trunk/c-icap@325 950cb5d8-81e4-40ab-8cb8-c719d5138fe1

---
+++ modules/dnsbl_tables.c
@@ -0,0 +1,112 @@
+#include "c-icap.h"
+#include "module.h"
+#include "lookup_table.h"
+#include "net_io.h"
+#include "cache.h"
+#include "debug.h"
+#include "common.h"
+
+
+int init_dnsbl_tables(struct ci_server_conf *server_conf);
+void release_dnsbl_tables();
+
+CI_DECLARE_MOD_DATA common_module_t module = {
+    "dnsbl_tables",
+    init_dnsbl_tables,
+    NULL,
+    release_dnsbl_tables,
+    NULL,
+};
+
+
+
+void *dnsbl_table_open(struct ci_lookup_table *table); 
+void  dnsbl_table_close(struct ci_lookup_table *table);
+void *dnsbl_table_search(struct ci_lookup_table *table, void *key, void ***vals);
+void  dnsbl_table_release_result(struct ci_lookup_table *table_data,void **val);
+
+struct ci_lookup_table_type dnsbl_table_type={
+    dnsbl_table_open,
+    dnsbl_table_close,
+    dnsbl_table_search,
+    dnsbl_table_release_result,
+    "dnsbl"
+};
+
+
+int init_dnsbl_tables(struct ci_server_conf *server_conf)
+{
+    return (ci_lookup_table_type_register(&dnsbl_table_type) != NULL);
+}
+
+void release_dnsbl_tables()
+{
+    ci_lookup_table_type_unregister(&dnsbl_table_type);
+}
+
+/***********************************************************/
+/*  bdb_table_type inmplementation                         */
+
+struct dnsbl_data {
+    char check_domain[CI_MAXHOSTNAMELEN+1];
+    ci_cache_t *cache;
+};
+
+void *dnsbl_table_open(struct ci_lookup_table *table)
+{
+    struct dnsbl_data *dnsbl_data;
+    if (strlen(table->path) >= CI_MAXHOSTNAMELEN ) {
+        ci_debug_printf(1, "dnsbl_table_open: too long domain name: %s\n",
+                        table->path);
+        return NULL;
+    }
+
+    dnsbl_data = malloc(sizeof(struct dnsbl_data));
+    if (!dnsbl_data) {
+        ci_debug_printf(1, "dnsbl_table_open: error allocating memory (dnsbl_data)!\n");
+        return NULL;
+    }
+    strcpy(dnsbl_data->check_domain, table->path);
+    dnsbl_data->cache = NULL;
+
+    table->data = dnsbl_data; 
+    return table->data;
+}
+
+void  dnsbl_table_close(struct ci_lookup_table *table)
+{
+     struct dnsbl_data *dnsbl_data = table->data;
+     table->data = NULL;
+     free(dnsbl_data);
+}
+
+void *dnsbl_table_search(struct ci_lookup_table *table, void *key, void ***vals)
+{
+    ci_sockaddr_t  addr;
+    char dnsname[CI_MAXHOSTNAMELEN];
+    char *server;
+    struct dnsbl_data *dnsbl_data = table->data;
+
+    if(table->key_ops != &ci_str_ops) {
+	ci_debug_printf(1,"Only keys of type string allowed in this type of table:\n");
+	return NULL;
+    }
+    server = (char *)key;
+    snprintf(dnsname, CI_MAXHOSTNAMELEN, "%s.%s", server, dnsbl_data->check_domain);
+    if(! ci_host_to_sockaddr_t(dnsname, &addr, AF_INET))
+	return NULL;
+    /*Are we interested for returning any data?
+      Well, some of the dns lists uses different ip addresses to categorize
+      hosts in various categories. TODO.
+     */
+    *vals = NULL;
+    return key;
+}
+
+void  dnsbl_table_release_result(struct ci_lookup_table *table,void **val)
+{
+    /*   struct ci_mem_allocator *allocator = table->allocator;
+	 allocator->free(allocator, val);
+    */
+    val = NULL;
+}
