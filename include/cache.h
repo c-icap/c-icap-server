bf5825f4d59bc51d533d7457a1d4c62a2c05fd9f
chtsanti <chtsanti@950cb5d8-81e4-40ab-8cb8-c719d5138fe1>
2009-03-17 17:34:18 +0000

    Fix error in arguments of ci_cache_build
    git-svn-id: https://svn.code.sf.net/p/c-icap/code/c-icap-server/trunk/c-icap@326 950cb5d8-81e4-40ab-8cb8-c719d5138fe1

--- include/cache.h
+++ include/cache.h
@@ -62,8 +62,8 @@ typedef struct ci_cache{
 } ci_cache_t;
 
 CI_DECLARE_FUNC(struct ci_cache *) ci_cache_build( unsigned int cache_size,
+                                                   unsigned int max_key_size,
 						   unsigned int max_object_size,
-						   unsigned int max_key_size,
 						   int ttl,
 						   ci_type_ops_t *key_ops,
 						   void *(copy_to_cache)(void *val, int *val_size, ci_mem_allocator_t *allocator),
