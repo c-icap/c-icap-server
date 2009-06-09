d1572da484d9abd5442282e35093c3abdcd54966
chtsanti <chtsanti@950cb5d8-81e4-40ab-8cb8-c719d5138fe1>
2009-06-09 21:20:52 +0000

    Use the icap-client c-icap api for icap-stretch utility
    git-svn-id: https://svn.code.sf.net/p/c-icap/code/c-icap-server/trunk/c-icap@386 950cb5d8-81e4-40ab-8cb8-c719d5138fe1

--- utils/icap-stretch.c
+++ utils/icap-stretch.c
@@ -31,10 +31,18 @@
 #include <sys/socket.h>
 #include <sys/ioctl.h>
 #include <signal.h>
-#include <ci_threads.h>
+
+#include "request.h"
+#include "ci_threads.h"
+#include "simple_api.h"
+#include "net_io.h"
+#include "cfg_param.h"
+#include "debug.h"
+
 
 
 /*GLOBALS ........*/
+int CONN_TIMEOUT = 30;
 char *servername;
 char *service;
 int threadsnum = 0;
@@ -109,160 +117,7 @@ static void sigint_handler(int sig)
 
 
 
-
-int icap_write(int fd, const void *buf, size_t count)
-{
-     int bytes = 0;
-     int remains = count;
-     char *b = (char *) buf;
-
-     while (remains > 0) {      //write until count bytes written
-          do {
-               bytes = write(fd, b, remains);
-          } while (bytes == -1 && errno == EINTR);
-
-          if (bytes < 0)
-               return bytes;
-          b = b + bytes;        //points to remaining bytes......
-          remains = remains - bytes;
-     }                          //Ok......
-     return count;
-}
-
-
-int readline(char *buf, int max, FILE * f)
-{
-     char c;
-     int i = 0;
-     while ((c = getc(f)) != EOF) {
-          buf[i] = c;
-          i++;
-          if (c == '\n' || i >= max)
-               return i;
-     }
-     return 0;
-}
-
-
-int icap_readline(int fd, char *buf, int BUFSIZE)
-{
-     int i = 0, readed = 0;
-     char c;
-     while ((readed = read(fd, &c, 1)) > 0 && c != '\n' && i < BUFSIZE) {
-          if (c == '\r') {
-               read(fd, &c, 1);
-               if (c == '\n')
-                    break;
-               buf[i++] = '\r';
-               buf[i++] = c;
-          }
-          else
-               buf[i++] = c;
-     }
-     buf[i] = '\0';
-     if (i == BUFSIZE) {
-          printf("Readline error. Skip until eol ......\n");
-          while (read(fd, &c, 1) > 0 && c != '\n');
-     }
-     return i;
-}
-
-
-
-struct buffer {
-     char *buf;
-     int size;
-};
-
-
-
-int readheaderresponce(int fd)
-{
-     char lbuf[512], *tmpstr;
-     int len, bytes, startbody = 0, remainbytes = -1;
-     int blocks = 0;
-     bytes = 0;
-     printf("\n\nRESPONCE: ");
-     while ((len = read(fd, lbuf, 512)) > 0) {
-          bytes += len;
-          lbuf[len] = '\0';
-
-          if ((tmpstr = strstr(lbuf, "null-body=")) != NULL) {
-               tmpstr += 10;
-               startbody = atoi(tmpstr);
-          }
-
-          if ((tmpstr = strstr(lbuf, "\r\n\r\n")) != NULL) {
-               remainbytes = bytes - (tmpstr + 4 - lbuf);
-               blocks++;
-          }
-
-
-//        printf("(BYTES=%d)\n%s",len,lbuf);
-//        printf("%s",lbuf);
-          printf
-              ("Read bytes: %d, start of body at: %d, remaining bytes: %d, blocks: %d\n",
-               bytes, startbody, remainbytes, blocks);
-          if (blocks == 2)
-               return bytes;
-     }
-     return 0;
-}
-
-#define SIZE_8K 8192
-int readallresponce(int fd, int *keepalive)
-{
-     char c, cprev, lbuf[SIZE_8K];
-     int len, remains, toread, i, totalbytes;
-     i = 0;
-     totalbytes = 0;
-     *keepalive = 1;
-     while (1) {
-          totalbytes += icap_readline(fd, lbuf, SIZE_8K);
-          if (i == 0 && strstr(lbuf, "Connection: close"))
-               *keepalive = 0;
-          if (strlen(lbuf) == 0)
-               i++;
-          if (i == 2)
-               break;
-     }
-
-     while (1) {
-          c = 0;
-          cprev = 0;
-          for (i = 0; !(c == '\n' && cprev == '\r'); i++) {
-               cprev = c;
-               if (i > 10 || read(fd, &c, 1) <= 0) {
-                    lbuf[i] = '\0';
-                    printf("Error reading socket %d.Exiting i:%d, lbuf:%s\n",
-                           fd, i, lbuf);
-                    return -1;
-               }
-               totalbytes += 1;
-               lbuf[i] = c;
-          }
-          len = strtol(lbuf, NULL, 16);
-//        printf("Going to read %d bytes\n",len);
-          if (len == 0) {
-               read(fd, &c, 1);
-               read(fd, &c, 1);
-               return totalbytes + 2;
-          }
-          totalbytes += len;
-          remains = len + 2;
-          while (remains > 0) {
-               toread = (remains > SIZE_8K ? SIZE_8K : remains);
-               if ((len = read(fd, lbuf, toread)) <= 0)
-                    return -1;
-               remains = remains - len;
-          }
-     }
-     return 1;
-}
-
-
-
-int threadjobreqmod(struct buffer *buf)
+int threadjobreqmod()
 {
      struct sockaddr_in addr;
      struct hostent *hent;
@@ -292,13 +147,7 @@ int threadjobreqmod(struct buffer *buf)
           }
 
           for (;;) {
-               if (icap_write(fd, buf->buf, buf->size) < 0) {
-                    printf("Connection closed, try again...\n");
-                    break;
-               }
-               readheaderresponce(fd);
-               if (!keepalive)
-                    break;
+	    /*Do a new request */
 //             sleep(4);
                sleep(1);
           }
@@ -308,138 +157,84 @@ int threadjobreqmod(struct buffer *buf)
 }
 
 
-const char *reqheader = "GET /manual/invoking.html HTTP/1.0\r\n"
-    "User-Agent: Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.4) Gecko/20030624\r\n"
-    "Accept: text/xml,application/xml,application/xhtml+xml,text/html;\r\n"
-    "Accept-Language: en-us,en;q=0.5\r\n"
-    "Accept-Encoding: gzip,deflate\r\n"
-    "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7\r\n"
-    "Keep-Alive: 300\r\n"
-    "Referer: http://barbarian.athens.home.gr/manual/\r\n"
-    "Via: 1.1 fortune.athens.home.gr:8080 (squid/2.5.STABLE1)\r\n"
-    "X-Forwarded-For: 192.168.1.4\r\n"
-    "Host: barbarian.athens.home.gr\r\n" "Cache-Control: max-age=259200\r\n";
 
 
-
-void buildreqheaders(char *buf)
-{
-     char lstr[128];
-     strcpy(buf, "REQMOD icap://");
-     strcat(buf, servername);
-     strcat(buf, ":1344/");
-     strcat(buf, service);      //modulename better
-     strcat(buf, " ICAP/1.0\r\n");
-     sprintf(lstr, "Encapsulated: req-hdr=0, null-body=%d\r\n",
-             strlen(reqheader));
-     strcat(buf, lstr);
-     strcat(buf, "Connection: keep-alive\r\n\r\n");
-     strcat(buf, reqheader);
-//     strcat(buf,"\r\n");
-     keepalive = 1;
-}
-
-
-
-void buildrespmodfile(FILE * f, char *buf)
+void build_headers(int fd, ci_headers_list_t *headers)
 {
      struct stat filestat;
      int filesize;
      char lbuf[512];
 //     struct tm ltime;
      time_t ltimet;
-     fstat(fileno(f), &filestat);
+     
+     ci_headers_add(headers, "Filetype: Unknown");
+     ci_headers_add(headers, "User: chtsanti");
+
+     fstat(fd, &filestat);
      filesize = filestat.st_size;
-     strcpy(lbuf, "HTTP/1.1 200 OK\r\n");
-     strcat(lbuf, "Date: ");
+
+     strcpy(lbuf, "Date: ");
      time(&ltimet);
      ctime_r(&ltimet, lbuf + strlen(lbuf));
      lbuf[strlen(lbuf) - 1] = '\0';
-     strcat(lbuf, "\r\n");
-     strcat(lbuf, "Last-Modified: ");
+     ci_headers_add(headers, lbuf);
+
+     strcpy(lbuf, "Last-Modified: ");
      ctime_r(&ltimet, lbuf + strlen(lbuf));
      lbuf[strlen(lbuf) - 1] = '\0';
-     strcat(lbuf, "\r\nContent-Length: ");
-     sprintf(lbuf + strlen(lbuf), "%d\r\n", filesize);
+     ci_headers_add(headers, lbuf);
 
-     strcat(lbuf, "\r\n");
+     sprintf(lbuf, "Content-Length: %d", filesize);
+     ci_headers_add(headers, lbuf);
 
-     strcpy(buf, "RESPMOD icap://");
-     strcat(buf, servername);
-     strcat(buf, ":1344/");
-     strcat(buf, service);      //modulename better
-     strcat(buf, " ICAP/1.0\r\n");
-     sprintf(buf + strlen(buf), "Encapsulated: res-hdr=0, res-body=%d\r\n",
-             strlen(lbuf));
-     strcat(buf, "Connection: keep-alive\r\n\r\n");
-     strcat(buf, lbuf);
-     keepalive = 1;
-//     printf("%s",buf);
 }
 
 
 
-int do_file(int fd, char *filename, int *keepalive)
+int fileread(void *fd, char *buf, int len)
 {
-     FILE *f;
-     char lg[10], lbuf[512];
-     int bytes, len, totalbytesout, totalbytesin;
-     unsigned int arand;
+     int ret;
+     ret = read(*(int *) fd, buf, len);
+     return ret;
+}
 
-     if ((f = fopen(filename, "r")) == NULL)
+int filewrite(void *fd, char *buf, int len)
+{
+     return len;
+}
+
+
+int do_file(ci_request_t *req, char *input_file, int *keepalive)
+{
+     int fd_in,fd_out;
+     int ret;
+     ci_headers_list_t *headers;
+
+     if ((fd_in = open(input_file, O_RDONLY)) < 0) {
+          ci_debug_printf(1, "Error opening file %s\n", input_file);
           return 0;
-
-     buildrespmodfile(f, lbuf);
-
-     if (icap_write(fd, lbuf, strlen(lbuf)) < 0)
-          return 0;
-
-
-//     printf("Sending file:\n");
-
-     totalbytesout = strlen(lbuf);
-     len = rand_r(&arand);
-     len++;
-     len = (int) ((((float) len) / RAND_MAX) * 510.0);
-     len = (len < 512 ? len : 512);
-     len = (len > 0 ? len : 1);
-     while ((len = fread(lbuf, sizeof(char), len, f)) > 0) {
-
-          totalbytesout += len;
-          bytes = sprintf(lg, "%X\r\n", len);
-          if (icap_write(fd, lg, bytes) < 0) {
-               printf("Error writing to socket:%s (after %d bytes).....\n",
-                      lg, totalbytesout);
-               req_errors_rw++;
-               return 0;
-          }
-          if (icap_write(fd, lbuf, len) < 0) {
-               printf("Error writing to socket.....\n");
-               req_errors_rw++;
-               return 0;
-          }
-          icap_write(fd, "\r\n", 2);
-//        printf("Sending chunksize :%d\n",len);
-
-          len = rand_r(&arand);
-          len++;
-          len = (int) ((((float) len) / RAND_MAX) * 510.0);
-          len = (len < 512 ? len : 512);
-          len = (len > 0 ? len : 1);
      }
-     icap_write(fd, "0\r\n\r\n", 5);
-     fclose(f);
+     fd_out = 0;
+     
+     headers = ci_headers_create();     
+     build_headers(fd_in, headers);
 
+     ret = ci_client_icapfilter(req,
+				CONN_TIMEOUT,
+				headers,
+				&fd_in,
+				(int (*)(void *, char *, int)) fileread,
+				&fd_out,
+				(int (*)(void *, char *, int)) filewrite);
+     close(fd_in);
 
-     //        printf("Done (%d bytes). Reading response.....\n",totalbytesout);
-     if ((totalbytesin = readallresponce(fd, keepalive)) < 0) {
-          printf("Read all response error;\n");
-          return -1;
-     }
+     keepalive=req->keepalive;
+
+     ci_headers_destroy(headers);
      // printf("Done(%d bytes).\n",totalbytes);
      ci_thread_mutex_lock(&statsmtx);
-     in_bytes_stats += totalbytesin;
-     out_bytes_stats += totalbytesout;
+     in_bytes_stats += req->bytes_in;
+     out_bytes_stats += req->bytes_out;
      ci_thread_mutex_unlock(&statsmtx);
 
      return 1;
@@ -448,33 +243,19 @@ int do_file(int fd, char *filename, int *keepalive)
 
 int threadjobsendfiles()
 {
-     struct sockaddr_in addr;
-     struct hostent *hent;
+     ci_request_t *req;
+     ci_connection_t *conn;
      int port = 1344;
-     int fd, indx, keepalive;
+     int indx, keepalive;
      int arand;
 
-     hent = gethostbyname(servername);
-     if (hent == NULL)
-          addr.sin_addr.s_addr = inet_addr(servername);
-     else
-          memcpy(&addr.sin_addr, hent->h_addr, hent->h_length);
-     addr.sin_family = AF_INET;
-     addr.sin_port = htons(port);
-
-
      while (1) {
-          fd = socket(AF_INET, SOCK_STREAM, 0);
-          if (fd == -1) {
-               printf("Error opening socket ....\n");
-               return -1;
-          }
 
-          if (connect(fd, (struct sockaddr *) &addr, sizeof(addr))) {
-               printf("Error connecting to socket .....\n");
-               exit(-1);
-               return -1;
-          }
+	  if (!(conn = ci_client_connect_to(servername, port, AF_INET))) {
+	    ci_debug_printf(1, "Failed to connect to icap server.....\n");
+	    exit(-1);
+	  }
+	  req = ci_client_request(conn, servername, service);
 
           for (;;) {
 
@@ -488,7 +269,7 @@ int threadjobsendfiles()
                ci_thread_mutex_unlock(&filemtx);
 
                keepalive = 0;
-               if (do_file(fd, FILES[indx], &keepalive) <= 0)
+               if (do_file(req, FILES[indx], &keepalive) <= 0)
                     break;
 
                ci_thread_mutex_lock(&statsmtx);
@@ -496,14 +277,15 @@ int threadjobsendfiles()
                arand = rand();  /*rand is not thread safe .... */
                ci_thread_mutex_unlock(&statsmtx);
 
-               if (keepalive == 0)
-                    break;
-
                if (_THE_END) {
                     printf("The end: thread dying\n");
-                    close(fd);
+		    ci_request_destroy(req);
                     return 0;
                }
+
+	       if (keepalive == 0)
+		 break;
+
                arand = (int) ((((double) arand) / (double) RAND_MAX) * 10.0);
                if (arand == 5 || arand == 7 || arand == 3) {    // 30% possibility ....
 //                  printf("OK, closing the connection......\n");
@@ -513,8 +295,10 @@ int threadjobsendfiles()
                usleep(500000);
 
 //             printf("Keeping alive connection\n");
+
+	       ci_client_request_reuse(req);
           }
-          close(fd);
+	  ci_request_destroy(req);
           usleep(1000000);
      }
 }
@@ -525,7 +309,6 @@ int threadjobsendfiles()
 int main(int argc, char **argv)
 {
      int i;
-     struct buffer buf;
 
      if (argc < 4) {
           printf
@@ -572,15 +355,14 @@ int main(int argc, char **argv)
 
      }
      else {                     //Construct reqmod......
-          buf.buf = malloc(1024 * sizeof(char));
-          buildreqheaders(buf.buf);
-          buf.size = strlen(buf.buf);
+       exit(-1);
+
 
           for (i = 0; i < threadsnum; i++) {
                printf("Create thread %d\n", i);
                ci_thread_create(&(threads[i]),
                                 (void *(*)(void *)) threadjobreqmod,
-                                (void *) &buf);
+                                (void *) NULL /*data*/);
                sleep(1);
           }
      }
