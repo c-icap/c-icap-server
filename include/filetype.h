d9a02a870587c9b409fb208d35e5082c1794b2a3
chtsanti <chtsanti@950cb5d8-81e4-40ab-8cb8-c719d5138fe1>
2010-05-03 22:49:23 +0000

    Bug fix: Bug 2992436 srv_Clamav crashes in must_scanned (file_type=-1 .....
    The the type in ci_data_type_groups was checked with maximum group number i
    not with the maximum type number 
    
    
    
    git-svn-id: https://svn.code.sf.net/p/c-icap/code/c-icap-server/trunk/c-icap@575 950cb5d8-81e4-40ab-8cb8-c719d5138fe1

--- include/filetype.h
+++ include/filetype.h
@@ -65,7 +65,7 @@ struct ci_magics_db{
 #define ci_magic_types_num(db) (db!=NULL?db->types_num:0)
 #define ci_magic_groups_num(db)(db!=NULL?db->groups_num:0)
 #define ci_data_type_name(db,i)(db!=NULL?db->types[i].name:NULL)
-#define ci_data_type_groups(db,i)(db!=NULL && i< db->groups_num && i >=0?db->types[i].groups:NULL)
+#define ci_data_type_groups(db,i)(db!=NULL && i< db->types_num && i >=0?db->types[i].groups:NULL)
 #define ci_data_type_descr(db,i)(db!=NULL && i< db->types_num && i >=0?db->types[i].descr:NULL)
 #define ci_data_group_name(db,i)(db!=NULL && i< db->groups_num && i >=0?db->groups[i].name:NULL)
 
