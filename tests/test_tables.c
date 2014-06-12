1a9c3378d136fdadfa7ce51763fdb0c2187af53e
chtsanti <chtsanti@950cb5d8-81e4-40ab-8cb8-c719d5138fe1>
2014-06-12 17:20:45 +0000

    call the post_init_module handler if it is available in test_tables
    git-svn-id: https://svn.code.sf.net/p/c-icap/code/c-icap-server/trunk/c-icap@1096 950cb5d8-81e4-40ab-8cb8-c719d5138fe1

--- tests/test_tables.c
+++ tests/test_tables.c
@@ -47,7 +47,10 @@ int load_module(const char *directive,const char **argv,void *setdata)
 	return 0;
     }
 
-    module->init_module(NULL);
+    if (module->init_module)
+        module->init_module(NULL);
+    if (module->post_init_module)
+        module->post_init_module(NULL);
 
     return 1;
 }
