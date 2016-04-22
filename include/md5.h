783d7325d009836175ac154384e8a6bc029d452c
chtsanti <chtsanti@950cb5d8-81e4-40ab-8cb8-c719d5138fe1>
2016-04-22 16:48:42 +0000

    Add 'extern "C"' on  md5 API definitions to allow compile with c++
    git-svn-id: https://svn.code.sf.net/p/c-icap/code/c-icap-server/branches/c_icap_0_4_x@1221 950cb5d8-81e4-40ab-8cb8-c719d5138fe1

--- include/md5.h
+++ include/md5.h
@@ -3,6 +3,11 @@
 
 #include "c-icap.h"
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 struct ci_MD5Context {
         uint32_t buf[4];
         uint32_t bits[2];
@@ -12,5 +17,9 @@ typedef struct ci_MD5Context ci_MD5_CTX;
 CI_DECLARE_FUNC(void) ci_MD5Init(struct ci_MD5Context *ctx);
 CI_DECLARE_FUNC(void) ci_MD5Update(struct ci_MD5Context *ctx, const unsigned char *buf, size_t len);
 CI_DECLARE_FUNC(void) ci_MD5Final(unsigned char digest[16], struct ci_MD5Context *ctx);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* !CI_MD5_H */
