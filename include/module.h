c1a5ff94c13efcbf6fe0c18a000dc4a6b92533f3
Frank Crawford <frank@crawford.emu.id.au>
2025-01-27 13:04:03 +1100

    Patch for GCC15 issues - BZ2339953

--- include/module.h
+++ include/module.h
@@ -96,7 +96,7 @@ typedef struct http_auth_method {
     void *(*create_auth_data)(const char *authorization_header,const char **username);
     void (*release_auth_data)(void *data);
     char *(*authentication_header)(ci_request_t *req);
-    void (*release_authentication_header)();
+    void (*release_authentication_header)(const char *auth_header);
     struct ci_conf_entry *conf_table;
 } http_auth_method_t;
 
